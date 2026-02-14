b8b920b45ef94282ca220e623ab6e12999d374e2
Joseph Collado <sourpina@gmail.com>
2026-02-13 22:33:33 -0500

    Added more scene reference setup on ready

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,12 +1,29 @@
 extends Node
 
+@onready var _host_game_button: Button = %HostGameButton
+@onready var _join_game_button: Button = %HostGameButton
+@onready var _join_button: Button = %HostGameButton
+@onready var _back_button: Button = %HostGameButton
+
+@onready var _main_panel: Panel = %MainPanel
+@onready var _join_panel: Panel = %JoinPanel
+
+@onready var _ip_line_edit: LineEdit = %IPLineEdit
+@onready var _port_line_edit: LineEdit = %PortLineEdit
+
 #region Setup
 
 func _ready() -> void:
-	%HostGameButton.pressed.connect(_on_host_game)
-	%JoinGameButton.pressed.connect(_on_join_game)
-	%ConnectButton.pressed.connect(_on_connect)
-	%BackButton.pressed.connect(_on_back)
+	_host_game_button.pressed.connect(_on_host_game)
+	_join_game_button.pressed.connect(_on_join_game)
+	_join_button.pressed.connect(_on_connect)
+	_back_button.pressed.connect(_on_back)
+	
+	_main_panel.visible = true
+	_join_panel.visible = false
+	
+	_ip_line_edit.text = ""
+	_port_line_edit.text = ""
 
 #endregion
 
