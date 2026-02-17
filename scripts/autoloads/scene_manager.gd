fc57fa68339a0e6e4524378a9090f648e7f5b619
Joseph Collado <sourpina@gmail.com>
2026-02-17 16:11:12 -0500

    Added function to load arena

--- scripts/autoloads/scene_manager.gd
+++ scripts/autoloads/scene_manager.gd
@@ -2,3 +2,6 @@ extends Node
 
 func load_character_select():
 	get_tree().change_scene_to_file("res://scenes/character_select.tscn")
+
+func load_arena():
+	get_tree().change_scene_to_file("res://scenes/arena.tscn")
