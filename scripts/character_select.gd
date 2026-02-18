977e3c7df852b3f55548f7400f76f18181b025e8
Joseph Collado <sourpina@gmail.com>
2026-02-18 15:10:04 -0500

    Changed rpc to also call on local client

--- scripts/character_select.gd
+++ scripts/character_select.gd
@@ -33,23 +33,28 @@ func _on_player_disconnected(id):
 
 # Sets this client's selected character on all clients
 func _on_character_selected(character: Util.Character):
-	MultiplayerManager.set_player_character(multiplayer.get_unique_id(), character)
 	_set_character.rpc(character)
-	
-	_local_character_name.text = _get_character_name(character)
-	# TODO: Set local character art here
 
 #endregion
 
 # Sets the sending client's selected character on this client
-@rpc("any_peer", "reliable")
+@rpc("call_local", "any_peer", "reliable")
 func _set_character(character: Util.Character):
 	var sender_id = multiplayer.get_remote_sender_id()
+	var sender_is_local_client = sender_id == multiplayer.get_unique_id()
 	print("%s selected %s" % [MultiplayerManager.players[sender_id].name, Util.Character.keys()[character]])
 	MultiplayerManager.set_player_character(sender_id, character)
+	_set_character_info(character, sender_is_local_client)
+
+func _set_character_info(character: Util.Character, is_local_client: bool):
+	var character_name = Util.Character.keys()[character]
 	
-	_remote_character_name.text = _get_character_name(character)
-	# TODO: Set remote character art here
+	if is_local_client:
+		_local_character_name.text = character_name
+		_local_character_art.texture = null
+	else:
+		_remote_character_name.text = character_name
+		_remote_character_art.texture = null
 
 # Returns the name associated with the given character enum
 # If the given enum is None, returns a default string
