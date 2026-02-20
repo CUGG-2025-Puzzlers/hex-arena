d8274a5019fac2d8a16fe608ba3760fd0b66466e
Joseph Collado <sourpina@gmail.com>
2026-02-20 16:09:58 -0500

    Created multiplayer input controller with directional input

---
+++ scripts/multiplayer_input.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var direction: Vector2
+
+func _ready() -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
