3cc87f4612e0ab3e225d1f5ac8fe794952cd5f46
Mouad Oulouali <mouadoune430@gmail.com>
2026-02-20 22:27:32 -0500

    end scene script that changes scene label to display winner name and button to swtich
    old player data is still in players so character select displays strart before both select character

---
+++ scenes/end_scene.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var winner_label : Label = $VBoxContainer/WinnerLabel
+@onready var play_again_btn : Button = $VBoxContainer/PlayAgainBtn
+
+
+func _ready() -> void:
+	winner_label.text = "%s. is the winner!" % SceneManager.winner_name
+	play_again_btn.pressed.connect(_on_play_again_button_pressed)
+	
+	
+func _on_play_again_button_pressed() -> void:
+	SceneManager.load_character_select()
+	
+	
+	
