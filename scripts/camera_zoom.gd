9690e3d8ecf0e0f2be931b5cb3ae58a789b22372
yakov-jacob <yakov.shulepov@gmail.com>
2026-02-22 18:26:10 -0500

    Disabled coordinates, redrawing the grid every frame for now,
    call after the camera zoom

--- scripts/camera_zoom.gd
+++ scripts/camera_zoom.gd
@@ -47,4 +47,9 @@ func _process(delta: float) -> void:
 	if dist_ratio > lower_bound:
 		new_zoom = max(0.73,1./(dist_ratio/upper_bound))
 
+	#if abs(zoom.x-new_zoom)>0.005:
+	#	HexCells.player_unique_instance.queue_redraw()
 	zoom = Vector2.ONE * ((new_zoom-zoom.x)*delta+zoom.x)
+	
+	# FOR NOW, LATER RENDER AND SAVE AS TEXTURE
+	HexCells.player_unique_instance.queue_redraw()
