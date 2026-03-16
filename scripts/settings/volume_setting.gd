8879ef6de39c22684451ec696451621b4155a6d2
Mouad Oulouali <mouadoune430@gmail.com>
2026-03-16 14:54:56 -0400

    making slider control volume

--- scripts/settings/volume_setting.gd
+++ scripts/settings/volume_setting.gd
@@ -19,3 +19,13 @@ func _ready() -> void:
 
 func _on_slide_value_changed(value: float):
 	_value_label.text = str(int(value))
+
+	# get "Master" audio bus (indx 0). Set volume
+	var bus_idx = AudioServer.get_bus_index("Master") 
+	
+	if value <= 0: # 0 = mute asnd 100 = blast
+		AudioServer.set_bus_mute(bus_idx, true)
+	else:
+		AudioServer.set_bus_mute(bus_idx, false)
+		# convert 0-100 slider range to decibels
+		AudioServer.set_bus_volume_db(bus_idx, linear_to_db(value / 100.0))
