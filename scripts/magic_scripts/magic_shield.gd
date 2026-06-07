d813f4f2d4c1474dc08b50b271db7284b82a105d
yakov-jacob <yakov.shulepov@gmail.com>
2026-06-07 03:47:48 -0400

    Split up shield implementation and added timer back in

--- scripts/magic_scripts/magic_shield.gd
+++ scripts/magic_scripts/magic_shield.gd
@@ -11,23 +11,26 @@ var glow_tween : Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	find_child("Shield Glow").texture = shield_animated_texture
-	
-	if player_id!=multiplayer.get_unique_id():
-		var pattern: Polygon2D = find_child("Shield Pattern")
-		var glow: Polygon2D = find_child("Shield Glow")
-		
-		var temp = pattern.texture
-		pattern.texture = glow.texture
-		glow.texture=temp
-	
 	visualize_shield()
+	recalculate_coll_shape()
+	create_and_start_glow()
 	
+	if HexCells.player_unique_instance:
+		start_lifetime_timer(lifetime)
+
+func start_lifetime_timer(time: float):
+	var timer : Timer = Timer.new()
+	timer.one_shot = true
+	timer.autostart = true
+	
+	timer.timeout.connect(fizzle)
+	
+	timer.wait_time = time
+	add_child(timer)
+
+func recalculate_coll_shape():
 	coll_shape.shape = HexCells.hex_polygon_shape
 	get_node("ShieldBody").add_child(coll_shape.duplicate())
-	
-	create_and_start_glow()
 
 func create_and_start_glow():
 	var start_point = randf()
@@ -39,13 +42,18 @@ func create_and_start_glow():
 			prog = 0.5 * (cos(2 * PI * prog) + 1)
 			shield_animated_texture.fill_to = Vector2.ONE * lerpf(0.6, 0.8, prog) * sqrt(2)
 	, start_point, start_point+[-1.,1.].pick_random(), 5)
-	glow_tween.loop_finished.connect(check_expired.unbind(1))
-
-func check_expired():
-	if glow_tween.get_total_elapsed_time() >= lifetime:
-		fizzle()
 
 func visualize_shield():
+	find_child("Shield Glow").texture = shield_animated_texture
+	
+	if player_id!=multiplayer.get_unique_id():
+		var pattern: Polygon2D = find_child("Shield Pattern")
+		var glow: Polygon2D = find_child("Shield Glow")
+		
+		var temp = pattern.texture
+		pattern.texture = glow.texture
+		glow.texture=temp
+	
 	var bounding_box: Rect2 = Rect2()
 	bounding_box.position = Vector2(HexCells.hex_shape[5].x,HexCells.hex_shape[0].y)
 	bounding_box.size = abs(bounding_box.position)*2
