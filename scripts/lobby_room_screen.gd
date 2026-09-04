5d8cd994d5c65a7fc1aa0399c9c5ae446190a8ad
PKhant78 <phonekhantkyawswa555@gmail.com>
2026-09-03 22:15:27 -0400

    Route EOS lobby flow through lobby room screen

--- scripts/lobby_room_screen.gd
+++ scripts/lobby_room_screen.gd
@@ -1,9 +1,10 @@
 extends Control
 
-
+@onready var host_card = %HostPlayerCard
+@onready var other_card = %OtherPlayerCard
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	host_card = MultiplayerManager.players[1].name
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
